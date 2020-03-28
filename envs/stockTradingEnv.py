540247eb0b82ee57ebe8eb44a7ba01be0c22707a
Akhil Singh Rana <akhilsinghrana@gmail.com>
2020-03-28 18:09:06 +0100

    Updates to gym Environments,  Step and Reward Scheme added to the environment

--- envs/stockTradingEnv.py
+++ envs/stockTradingEnv.py
@@ -1,7 +1,7 @@
 import os,sys
 from importlib import reload
 import numpy as np
-
+import random
 
 #'Import gym', we will create a small wrapper with gym environment recommendations
 import gym
@@ -20,23 +20,28 @@ class TradingEnvironment(gym.Env):
         
         #we allow movement only 1 element far ... so 8 neighboring elements are max possible elements to move to
         # 9 possible actions +1 to encode no movement
-        self.action_space = gym.spaces.Discrete(self.env_utils.num_actions)
+        self.action_space = gym.spaces.Box(low= np.array([0, 0]) , high= np.array([self.env_utils.num_actions, 1]), dtype=np.float32)
+        self.ACOUNT_BALANCE = self.env_utils.ACOUNT_BALANCE
+        self.reward_range = (0, self.ACOUNT_BALANCE*10000)
         
         
         self.done=False
         self.episodeCounter = 0 #Keep track of number of elapsed episodes during the run
         self.episodeLength = 0 #Keep track of the length of the episode, might be important for the reward scheme
+        self.max_episode_length = 2*364*24
         
         #Keep track of the rewards received by the agent over the time frame
         self.step_reward = 0
         self.episode_reward = 0
+
+        #Stock Share options for agent
+        self.SHARES_HELD = 0 #Number of shares Held in total
         
         print("Env Initialized")
     
-    def setObservation(self):
+    def setObservation(self, mode="step"):
         # generate the observation space either directly form the online stock library or local file
-        
-        self.observation_space = self.env_utils._getNextObservation()
+        self.observation_space = self.env_utils._getNextObservation(mode=mode)
 
     def reset(self):
         """
@@ -46,10 +51,19 @@ class TradingEnvironment(gym.Env):
         self.episodeLength=0
         self.episodeCounter += 1
         self.episode_reward = 0
+
+        #Stock Share options for agent
+        self.ACOUNT_BALANCE = self.env_utils.ACOUNT_BALANCE
+        self.SHARES_HELD = 0 #Number of shares bought in this episode
+        self.SHARES_SOLD = 0 #Number of shares bought in this episode
+
+        self.MONEY_SPENT = 0
+        self.MONEY_EARNED = 0
+        self.net_worth = self.max_net_worth = self.ACOUNT_BALANCE
         
         self.done = False
         
-        self.setObservation() 
+        self.setObservation(mode="reset") 
         # self.render(mode="graphics")
         return self.observation_space
             
@@ -70,38 +84,61 @@ class TradingEnvironment(gym.Env):
         self.episodeLength+=1
         # Perform an action by the agent
         self.performAction(action)
-        self.episode_reward+=self.step_reward
+        self.episode_reward += self.step_reward
         
-        self.done=False # Change to True when the environment is finished #TO-DO
+        # Change to True when the net worth is 0
+        self.done= self.net_worth<=0 
         self.setObservation()
         info = {} #Generate extra information for debug
-        return self.observation_space, self.step_reward, self.done, {}
+        return self.observation_space, self.step_reward, self.done, info
     
     def performAction(self, action):
         #### Maps an Algorithm's action to the Agent's action
         assert (action in self.action_space), "Oh, no action is invalid, check the action space of the environment"
+        action_type = action[0]
+        amount = action[1]
         self.step_reward = -0.01 # Define how much reward has to be assinged for the action
-       
-        
-        if action == 0:
+        # Set the current price to a random price within the time step
+        current_price = random.uniform(self.env_utils.open, self.env_utils.close)
+            
+        if action_type < 1:
             # HOLD
             pass
-        elif action == 1:
-            # BUY
-            pass
-        elif action == 2:
-            # SELL
-            pass
+        elif action_type < 2:
+            # BUY a percentage amount
+            total_possible = self.ACOUNT_BALANCE / current_price
+            shares_bought = total_possible * amount
+            additional_cost = shares_bought * current_price
+            self.MONEY_SPENT += additional_cost
+            self.ACOUNT_BALANCE -= additional_cost
+            self.SHARES_HELD += shares_bought
             
+        elif action_type < 3:
+            # SELL a percentage amount
+            shares_sold = self.SHARES_HELD * amount 
+            self.ACOUNT_BALANCE += shares_sold * current_price
+            self.SHARES_HELD -= shares_sold
+            self.SHARES_SOLD += shares_sold
+            self.MONEY_EARNED += shares_sold * current_price
+
+            
+        self.netWorth = self.ACOUNT_BALANCE + self.SHARES_HELD * current_price
+        if self.net_worth > self.max_net_worth:
+            self.max_net_worth = self.net_worth
+        
+        print("NET WORTH IN AMOUNT", self.net_worth)
         # Calculate the rewards now
+        self.calculateRewards()
 
             
     def calculateRewards(self):
         '''
-        Design the reward Scheme here
+        Reward Scheme focussed on account balance
         '''
         #Reward calculations
-        reward = 0
+        delay_modifier = (self.episodeLength / self.max_episode_length)
+        reward = self.ACOUNT_BALANCE * delay_modifier
+        
         return reward
 
 
--- util/environmentUtils.py
+++ util/environmentUtils.py
@@ -1,6 +1,7 @@
 #Import the necessary packages
 import yfinance as yf
 import pandas as pd
+import random
 
 from collections import deque
 class EnvironmentUtils():
@@ -15,16 +16,22 @@ class EnvironmentUtils():
         
         self.MAX_SHARE_PRICE = None #Max price of the share used to normalize the data
         self.MAX_NUM_SHARES = None #Max number of Shares/Volume that the data can have
+        self.init_loc = None        #Initial lcoation where the observation will start from, radnom every episode
+        self.loc = self.init_loc
 
         #Observation based variables for the environment defined below
         self.n_days_obs = 5 #Number of days to keep in the Observation Agent will have history for n days
         self.features=["Open", "Low", "High", "Close", "Volume"]
         self.num_features_to_consider = len(self.features) #Number of features that agent get's everyday of every hour
         self.filled_Obs = deque(maxlen=self.n_days_obs)
-        self.num_actions = 3 # Number of actions agent should sample from
+        
+        #Action based variables
+        self.num_actions = 3 # Number of actions agent should sample from, Buy, Hold Sell
+        self.ACOUNT_BALANCE = 50000
+         
         self.agent_reaction_time = 1 #How often does the agent reacts every n hours or days, necesaary to define the length of a step
 
-    def _getNextObservation(self, loc=0, getData="online"):
+    def _getNextObservation(self, getData="online", mode="step"):
         """
             Gets the observation for the next step from the data Frame we support Pandas for now.
             The data can be either downloaded live online from yahooFinace library or can be read from CSV file
@@ -43,17 +50,40 @@ class EnvironmentUtils():
                 raise NotImplementedError
                 
         
-        #Read the data at specified location depends on the episode length of the environment
-        self.MAX_NUM_SHARES = self.pandasData["Volume"].max()
-        self.MAX_SHARE_PRICE = self.pandasData["High"].max()
-        obs = self.pandasData.iloc[loc]
-        print(obs, self.pandasData.ndim)
-        print(self.filled_Obs)
-        obs = obs[self.features]
-        print(obs)
+        if mode=="reset":
+            self.MAX_NUM_SHARES = self.pandasData["Volume"].max()
+            self.MAX_SHARE_PRICE = self.pandasData["High"].max()
+            self.len_data = self.pandasData.shape[0]
+            self.init_loc = random.randint(0, self.len_data) 
+            self.loc=self.init_loc
+            print("Starting the episode from day ==>",self.loc)
+
+        
+        if self.loc==self.len_data-1:
+            self.loc=0
+        
+    
         if len(self.filled_Obs)<self.n_days_obs:
+            while len(self.filled_Obs)<self.n_days_obs:
+                obs = self.pandasData.iloc[self.loc]
+                self.open = obs["Open"].round(2)
+                self.close = obs["Close"].round(2)
+                obs = obs[self.features].round(2)
+                
+                
+                self.loc+=1
+                if self.loc==self.len_data-1:
+                    self.loc=0
+                self.filled_Obs.append([obs.values])
+        else:
+            obs = self.pandasData.iloc[self.loc]
+            obs = obs[self.features].round(2)
+            self.loc+=1
+            if self.loc==self.len_data-1:
+                self.loc=0
+        
             self.filled_Obs.append([obs.values])
-        print(self.filled_Obs, len(self.filled_Obs))
+        
         return obs
     
 
